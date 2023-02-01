321750cfe14c58d58b2ca89c92921d32cad9905a
gdjenrio <enriquemanuelriosperez@gmail.com>
2023-02-01 13:10:58 -0600

    Se deja lo mas pulido el codigo

--- ingreso_carpetas.py
+++ ingreso_carpetas.py
@@ -4,59 +4,55 @@ import base_datos as db
 import manejo as manage
 lista_existente = []
 lista_no_existente = []
-# from base_datos import consulta 
-content = os.listdir('/Users/gdjenrio/Documents/Almacen Central/Acciones-CLCA')
-direccion_reportes= '//gdjnt335/Reportes/'
+mandados = 0
+no_mandados = 0
+archivos_nuevos = 0 
+c_origen= "//gdjnt335/Reportes/"
+c_utilizacion = "Utilization Daily"
+lista_c_utilizacion = os.listdir(c_origen)
+os.chdir(c_origen)
+print("La direccion {}" .format(os.getcwd()))
 
-carpeta_utilizacion = "Utilization Daily"
-lista_carpetas = os.listdir('//gdjnt335/Reportes/')
-# \\gdjnt335\Reportes\N4_L05\Utilization Daily
-os.chdir(direccion_reportes)
-print("la direccion {}" .format(os.getcwd()))
-
-for x in lista_carpetas:
+for x in lista_c_utilizacion:
     print(x)
-    direccion_carpetas = (os.path.join(direccion_reportes,x))
-    # print(direccion_carpetas)
+    direccion_carpetas = (os.path.join(c_origen,x))
     os.chdir(direccion_carpetas)
-    # print("la direccion {}" .format(os.getcwd()))
-    if os.path.exists(carpeta_utilizacion):
+    if os.path.exists(c_utilizacion):
+
         lista_existente.append(x)
-        
-        direccion_carpetas_utili = (os.path.join(direccion_carpetas + "/",carpeta_utilizacion))
-        print(direccion_carpetas_utili)
-        os.chdir(direccion_carpetas_utili)
-        fechas = os.listdir(".")
-        # print("la direccion {}" .format(os.getcwd()))
-        # print("Las fechas que tiene el archivo: {}".format(fechas))
-        for carpetas_fechas in fechas:
-            dd = os.path.join(direccion_carpetas_utili + "/", carpetas_fechas)
-            os.chdir(dd)
-            # print("la direccion {}" .format(os.getcwd()))
-            archivos_modificacion = os.listdir(".")
-            #print("Archivos de fecha {} : {}" .format(carpetas_fechas,archivos_modificacion ))
-            validacion = db.consulta(x,carpetas_fechas)
-            # print("Validacion si existe de algun registro {}" .format(validacion))
+        no_mandados = no_mandados+1
+        direccion_c_utilizacion = (os.path.join(direccion_carpetas + "/",c_utilizacion))
+        print(direccion_c_utilizacion)
+        os.chdir(direccion_c_utilizacion)
+        c_fechas = os.listdir(".")
+
+        for fechas in c_fechas:
+            direccion_fechas = os.path.join(direccion_c_utilizacion + "/", fechas)
+            os.chdir(direccion_fechas)
+            lista_archivos = os.listdir(".")
+            validacion = db.consulta(x,fechas)
             if validacion == 0:
                 print("Archivo no existente")
-                for rr in archivos_modificacion:
-                    if "Utilization" in rr:
-                        nombre_archivo = rr
+                for file_name in lista_archivos:
+                    if "Utilization" in file_name:
+                        nombre_archivo = file_name
                         break
-                full_path = os.path.join(dd + "/", nombre_archivo)
+                full_path = os.path.join(direccion_fechas + "/", nombre_archivo)
                 print("El archivo que se mandara {}". format(nombre_archivo))
                 print("Direccion completa para mandar el archivo: \n{}".format(full_path))
-                manage.datos_archivos(full_path,nombre_archivo)
-                
+                archivos_nuevos = archivos_nuevos+1
+                #direccion completa al archivo para ingrsar los datos , x  es el nombre de la carpeta en la que se esta trabajando,
+                # fechas es el nombre de la carpeta que tiene fecha  
+                manage.datos_archivos(full_path,x,fechas)
             else:
-                print("Ya se mando el archivo")       
-            os.chdir(direccion_carpetas_utili)
-            # print("la direccion despues{}" .format(os.getcwd()))
-
+                print("Ya se mando el archivo")
+                mandados = mandados+1       
+            os.chdir(direccion_c_utilizacion)
     else:
         lista_no_existente.append(x)
-        print("No se tiene registro de esa carpeta")
+        print("No se tiene la carpeta {}" .format(c_utilizacion))
 
 
-print("De las {} carpetas, {} si tienen el archivo y {} no la tienen " .format(len(lista_carpetas),len(lista_existente ), len(lista_no_existente)))
+print("De las {} carpetas, {} si tienen el archivo y {} no la tienen " .format(len(lista_c_utilizacion),len(lista_existente ), len(lista_no_existente)))
+print("Se reportaron {} archivos nuevos y se tenian {}" .format(archivos_nuevos, mandados))
 print("Carpetas que no tienen el archivo: {}" .format(lista_no_existente))
--- manejo.py
+++ manejo.py
@@ -1,42 +1,21 @@
 
 import pandas as pd 
-import os 
 from matplotlib import pyplot as plt
 import  base_datos as db
 
-#direccion, nombre_archivo
-def datos_archivos(direccion , gg):
-    gg2 = gg.split("@")
-    nombre = gg2[1]
-    _Name = nombre.split(".")
-    Folder_Name = _Name[0]
-    nombre = nombre.replace("_", ".")
-    nombre_seccio = nombre.split(".")
-#aca tambbien 
-    nave_linea = nombre_seccio[0].split("_")
-#lo que estoy cambiarndo
-    # print("La nave {} y linea {}".format(nombre_seccio[0], nombre_seccio[1] ))
-    fecha = nombre_seccio[2]
-    fecha = fecha[0:8]
-    fecha_ral ="".join((fecha[0:4], '-',fecha[4:6], '-',fecha[6:8]) )
-    # print("La fecha del archivo es: {}". format(fecha_ral))
 
-    # datos = pd.read_csv('datos/Utilization@N4_L05.202212130000.202212140000.csv')
+                                            
+def datos_archivos(direccion , N_L, fecha):
+    n,l = N_L.split("_")
     datos = pd.read_csv(direccion)
-    # datos = pd.read_csv("//gdjnt335/Reportes/N1_L01A/Utilization Daily/2022-12-13/Utilization@N1_L01A.202212130000.202212140000.csv")
-    # print(datos)
     modulos = datos.columns.values
     modulos = modulos[1:]
-    # print(modulos)
     dd = datos.set_index("Module")
     trans = dd.transpose() 
-
-    trans = trans.assign(Line = nombre_seccio[1])
-    trans = trans.assign(Building = nombre_seccio[0])
-    trans = trans.assign(Date = fecha_ral)  
-    trans = trans.assign(Folder = Folder_Name)
-    # print("Despues de asignar el index")
-
+    trans = trans.assign(Line = l)
+    trans = trans.assign(Building = n)
+    trans = trans.assign(Date = fecha)  
+    trans = trans.assign(Folder = N_L)
     df_reset = trans.reset_index()
     for x in modulos:
         m3 = df_reset.loc[df_reset['index']== x]
@@ -44,5 +23,4 @@ def datos_archivos(direccion , gg):
         #nononono print(m4)
         for y in m4:
             # print(y)
-            
             db.insertar_procedires(y)
